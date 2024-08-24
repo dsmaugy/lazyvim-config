f3979cb961d90fa01a6920d353735a95acc00d13
Darwin Do <darwin78913@gmail.com>
2024-08-24 00:59:51 -0400

    Add theme, mason configs, and treesitter config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    -- optional = true,
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(
+          opts.ensure_installed,
+          { "clangd", "gopls", "actionlint", "dockerfile-language-server", "ruff" }
+        )
+      end
+    end,
+  },
+}
